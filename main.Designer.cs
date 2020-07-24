50205b6c7dce4b2e96dc44a0b2e34e19aae3d8b8
btframework <btframework@gmail.com>
2020-07-24 23:27:35 +0300

    Initial commit

---
+++ main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PairedGatt
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnect = new System.Windows.Forms.Button();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.btDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(12, 12);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(75, 23);
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(12, 41);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(776, 394);
+            this.lbLog.TabIndex = 1;
+            // 
+            // btDisconnect
+            // 
+            this.btDisconnect.Location = new System.Drawing.Point(93, 12);
+            this.btDisconnect.Name = "btDisconnect";
+            this.btDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btDisconnect.TabIndex = 2;
+            this.btDisconnect.Text = "Disconnect";
+            this.btDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDisconnect);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.btConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "fmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paired GATT devices communication";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.fmMain_FormClosed);
+            this.Load += new System.EventHandler(this.fmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.Button btDisconnect;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using wclCommon;
+using wclCommunication;
+using wclBluetooth;
+
+namespace PairedGatt
+{
+    public partial class fmMain : Form
+    {
+        private wclBluetoothManager FManager;
+        private wclGattClient FClient;
+
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            lbLog.Items.Clear();
+
+            Int32 Res = FManager.Open();
+            if (Res != wclErrors.WCL_E_SUCCESS)
+                lbLog.Items.Add("Bluetooth Manager Open Failed: 0x" + Res.ToString("X8"));
+        }
+
+        private void fmMain_Load(object sender, EventArgs e)
+        {
+            FManager = new wclBluetoothManager();
+            FManager.AfterOpen += FManager_AfterOpen;
+            FManager.BeforeClose += FManager_BeforeClose;
+
+            FClient = new wclGattClient();
+            FClient.ConnectOnRead = true;
+            FClient.OnConnect += FClient_OnConnect;
+            FClient.OnDisconnect += FClient_OnDisconnect;
+        }
+
+        private void FManager_BeforeClose(object sender, EventArgs e)
+        {
+            lbLog.Items.Add("Bluetooth manager has been closed.");
+        }
+
+        private void FClient_OnDisconnect(object Sender, int Reason)
+        {
+            lbLog.Items.Add("GATT client disconnected");
+        }
+
+        private void FClient_OnConnect(object Sender, int Error)
+        {
+            if (Error != wclErrors.WCL_E_SUCCESS)
+            {
+                lbLog.Items.Add("Connect failed: 0x" + Error.ToString("X8"));
+                FManager.Close();
+            }
+            else
+            {
+                wclGattService[] Services;
+                Int32 Res = FClient.ReadServices(wclGattOperationFlag.goReadFromCache, out Services);
+                if (Res != wclErrors.WCL_E_SUCCESS)
+                    lbLog.Items.Add("Read services failed: 0x" + Res.ToString("X8"));
+                else
+                {
+                    foreach (wclGattService s in Services)
+                        lbLog.Items.Add(s.ToString());
+
+                    FClient.Disconnect();
+                }
+                FManager.Close();
+            }
+        }
+
+        private void FManager_AfterOpen(object sender, EventArgs e)
+        {
+            lbLog.Items.Add("Bluetooth Manager has been opened");
+
+            if (FManager.Count == 0)
+            {
+                lbLog.Items.Add("No Bluetooth radio found");
+                FManager.Close();
+            }
+            else
+            {
+                wclBluetoothRadio Radio = FManager[0];
+                lbLog.Items.Add("Found " + Radio.ApiName + " Bluetooth radio");
+
+                Int64[] Devices;
+                Int32 Res = Radio.EnumPairedDevices(out Devices);
+                if (Res != wclErrors.WCL_E_SUCCESS)
+                    lbLog.Items.Add("Enumerate paired devices failed: 0x" + Res.ToString("X8"));
+                else
+                {
+                    if (Devices == null || Devices.Length == 0)
+                    {
+                        lbLog.Items.Add("Not paired devices found");
+                        FManager.Close();
+                    }
+                    else
+                    {
+                        Int64 Address = 0;
+                        foreach (Int64 a in Devices)
+                        {
+                            wclBluetoothDeviceType Type;
+                            Res = Radio.GetRemoteDeviceType(a, out Type);
+                            if (Res == wclErrors.WCL_E_SUCCESS && Type == wclBluetoothDeviceType.dtBle)
+                            {
+                                Address = a;
+                                break;
+                            }
+                        }
+
+                        if (Address == 0)
+                        {
+                            lbLog.Items.Add("No BLE device found");
+                            FManager.Close();
+                        }
+                        else
+                        {
+                            FClient.Address = Address;
+                            Res = FClient.Connect(Radio);
+                            if (Res != wclErrors.WCL_E_SUCCESS)
+                                lbLog.Items.Add("Connect failed; 0x" + Res.ToString("X8"));
+                        }
+                    }
+                }
+                
+                if (Res != wclErrors.WCL_E_SUCCESS)
+                    FManager.Close();
+            }
+        }
+
+        private void fmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FClient.Disconnect();
+            FManager.Close();
+        }
+    }
+}
